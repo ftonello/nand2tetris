b2cb37ed3ab44adc26868bb14265ea3b5e11a6e2
Felipe F. Tonello <eu@felipetonello.com>
2014-12-29 12:38:46 -0800

    07: Implement gt, lt and eq arithmetic commands
    Signed-off-by: Felipe F. Tonello <eu@felipetonello.com>

--- projects/07/code.cpp
+++ projects/07/code.cpp
@@ -4,6 +4,8 @@
 #include <fstream>
 #include <map>
 
+#include <boost/format.hpp>
+
 using namespace vm;
 
 class code_p {
@@ -24,6 +26,7 @@ private:
 	std::ofstream m_file;
 	std::map<std::string, command_function> m_push_pop_function;
 	std::map<std::string, arithmetic_function> m_arithmetic_function;
+	std::size_t m_label_count;
 
 	void push_pop_constant(vm::command_type cmd, uint16_t index);
 
@@ -36,6 +39,12 @@ private:
 	void arithmetic_and();
 	void arithmetic_or();
 	void arithmetic_not();
+
+	std::string label_create();
+	template<typename T>
+	void label_add(const T &label);
+	template<typename T>
+	void label_at(const T &label);
 };
 
 code::code(const std::string &file)
@@ -71,7 +80,8 @@ void code::write_push_pop(command_type cmd, const std::string &segment, uint16_t
 /************** Private Class **************/
 
 code_p::code_p(const std::string &file)
-	: m_file(file, std::ofstream::out)
+	: m_file(file, std::ofstream::out),
+	  m_label_count(0)
 {
 	m_push_pop_function = {
 		{ "constant", &code_p::push_pop_constant },
@@ -112,13 +122,13 @@ void code_p::eval_arithmetic(const std::string &cmd)
 
 void code_p::sp_inc()
 {
-	m_file << "@SP" << std::endl;
+	label_at("SP");
 	m_file << "M=M+1" << std::endl;
 }
 
 void code_p::sp_dec()
 {
-	m_file << "@SP" << std::endl;
+	label_at("SP");
 	m_file << "M=M-1" << std::endl;
 }
 
@@ -127,9 +137,9 @@ void code_p::sp_dec()
 // The constant segment can be only used for push commands.
 void code_p::push_pop_constant(command_type cmd, uint16_t index)
 {
-	m_file << "@" << index << std::endl;
+	label_at(index);
 	m_file << "D=A" << std::endl;
-	m_file << "@SP" << std::endl;
+	label_at("SP");
 	m_file << "A=M" << std::endl;
 	m_file << "M=D" << std::endl;
 }
@@ -152,7 +162,7 @@ void code_p::push_pop_constant(command_type cmd, uint16_t index)
 // X + Y
 void code_p::arithmetic_add()
 {
-	m_file << "@SP" << std::endl;
+	label_at("SP");
 	m_file << "AM=M-1" << std::endl;
 	m_file << "D=M" << std::endl;
 	m_file << "A=A-1" << std::endl;
@@ -162,7 +172,7 @@ void code_p::arithmetic_add()
 // X - Y
 void code_p::arithmetic_sub()
 {
-	m_file << "@SP" << std::endl;
+	label_at("SP");
 	m_file << "AM=M-1" << std::endl;
 	m_file << "D=M" << std::endl;
 	m_file << "A=A-1" << std::endl;
@@ -172,7 +182,7 @@ void code_p::arithmetic_sub()
 // -Y
 void code_p::arithmetic_neg()
 {
-	m_file << "@SP" << std::endl;
+	label_at("SP");
 	m_file << "A=M-1" << std::endl;
 	m_file << "M=-M" << std::endl;
 }
@@ -181,24 +191,110 @@ void code_p::arithmetic_neg()
 // X = (X == Y)
 void code_p::arithmetic_eq()
 {
+	std::string eq_label = label_create();
+	std::string neq_label = label_create();
+	std::string end_label = label_create();
+
+	label_at("SP");
+	m_file << "AM=M-1" << std::endl;
+	m_file << "D=M" << std::endl;
+	m_file << "A=A-1" << std::endl;
+	m_file << "D=M-D" << std::endl;
+
+	label_at(eq_label);
+	m_file << "D;JEQ" << std::endl;
+
+	label_at(neq_label);
+	m_file << "D;JNE" << std::endl;
+
+	label_add(eq_label);
+	label_at("SP");
+	m_file << "A=M-1" << std::endl;
+	m_file << "M=-1" << std::endl;
+	label_at(end_label);
+	m_file << "0;JMP" << std::endl;
+
+	label_add(neq_label);
+	label_at("SP");
+	m_file << "A=M-1" << std::endl;
+	m_file << "M=0" << std::endl;
+
+	label_add(end_label);
 }
 
 // 0xffff (-1) is true, and 0x0000 is false
 // X = (X > Y)
 void code_p::arithmetic_gt()
 {
+	std::string gt_label = label_create();
+	std::string le_label = label_create();
+	std::string end_label = label_create();
+
+	label_at("SP");
+	m_file << "AM=M-1" << std::endl;
+	m_file << "D=M" << std::endl;
+	m_file << "A=A-1" << std::endl;
+	m_file << "D=M-D" << std::endl;
+
+	label_at(gt_label);
+	m_file << "D;JGT" << std::endl;
+
+	label_at(le_label);
+	m_file << "D;JLE" << std::endl;
+
+	label_add(gt_label);
+	label_at("SP");
+	m_file << "A=M-1" << std::endl;
+	m_file << "M=-1" << std::endl;
+	label_at(end_label);
+	m_file << "0;JMP" << std::endl;
+
+	label_add(le_label);
+	label_at("SP");
+	m_file << "A=M-1" << std::endl;
+	m_file << "M=0" << std::endl;
+
+	label_add(end_label);
 }
 
 // 0xffff (-1) is true, and 0x0000 is false
 // X = (X < Y)
 void code_p::arithmetic_lt()
 {
+	std::string lt_label = label_create();
+	std::string ge_label = label_create();
+	std::string end_label = label_create();
+
+	label_at("SP");
+	m_file << "AM=M-1" << std::endl;
+	m_file << "D=M" << std::endl;
+	m_file << "A=A-1" << std::endl;
+	m_file << "D=M-D" << std::endl;
+
+	label_at(lt_label);
+	m_file << "D;JLT" << std::endl;
+
+	label_at(ge_label);
+	m_file << "D;JGE" << std::endl;
+
+	label_add(lt_label);
+	label_at("SP");
+	m_file << "A=M-1" << std::endl;
+	m_file << "M=-1" << std::endl;
+	label_at(end_label);
+	m_file << "0;JMP" << std::endl;
+
+	label_add(ge_label);
+	label_at("SP");
+	m_file << "A=M-1" << std::endl;
+	m_file << "M=0" << std::endl;
+	label_add(end_label);
 }
 
 // X AND Y
 void code_p::arithmetic_and()
 {
-	m_file << "@SP" << std::endl;
+	label_at("SP");
 	m_file << "AM=M-1" << std::endl;
 	m_file << "D=M" << std::endl;
 	m_file << "A=A-1" << std::endl;
@@ -208,7 +304,7 @@ void code_p::arithmetic_and()
 // X OR Y
 void code_p::arithmetic_or()
 {
-	m_file << "@SP" << std::endl;
+	label_at("SP");
 	m_file << "AM=M-1" << std::endl;
 	m_file << "D=M" << std::endl;
 	m_file << "A=A-1" << std::endl;
@@ -218,7 +314,28 @@ void code_p::arithmetic_or()
 // NOT Y
 void code_p::arithmetic_not()
 {
-	m_file << "@SP" << std::endl;
+	label_at("SP");
 	m_file << "A=M-1" << std::endl;
 	m_file << "M=!M" << std::endl;
+}
+
+/************** Label **************/
+
+std::string code_p::label_create()
+{
+	boost::format label_fmt("VMLABEL%d");
+	label_fmt % m_label_count++;
+	return label_fmt.str();
+}
+
+template<typename T>
+void code_p::label_add(const T &label)
+{
+	m_file << "(" << label << ")" << std::endl;
+}
+
+template<typename T>
+void code_p::label_at(const T &label)
+{
+	m_file << "@" << label << std::endl;
 }
