c39ef0bf5e895e264c7789867cfefb97d0f26a81
Felipe F. Tonello <eu@felipetonello.com>
2014-12-31 16:06:59 -0800

    07: Support directory lookup
    Signed-off-by: Felipe F. Tonello <eu@felipetonello.com>

--- projects/07/vm.cpp
+++ projects/07/vm.cpp
@@ -2,18 +2,26 @@
  * Jack VM implementation
  *
  * To compile:
- * g++ vm.cpp -std=c++11 -Wall -o vm
+ *   $ mkdir build
+ *   $ cd $_
+ *   $ cmake ..
+ *   $ make
  */
 
 #include "command_type.h"
 #include "parser.h"
 #include "code.h"
 
+#include <algorithm>
 #include <iostream>
+#include <cstdlib>
 
-int main(int argc, char *argv[])
+#include <boost/filesystem.hpp>
+
+namespace fs = boost::filesystem;
+
+static void translate_file(const std::string &file_name)
 {
-	std::string file_name(argv[1]);
 	std::string asm_file_name(file_name.substr(0, file_name.rfind(".")).append(".asm"));
 	vm::parser p(file_name);
 	vm::code c(asm_file_name);
@@ -27,12 +35,46 @@ int main(int argc, char *argv[])
 		case vm::command_type::c_arithmetic:
 			c.write_arithmetic(p.arg1());
 			break;
+		case vm::command_type::none:
+			break;
 		default:
-			std::cerr << "Error: Command not implemented yet." << std::endl;
+			std::cerr << "Error: Command (" << static_cast<int>(p.command()) << ") not implemented yet." << std::endl;
 		}
 	}
 
 	std::cout << "Writen Hack assembly to: " << asm_file_name << std::endl;
+}
+
+static void abort_with_usage(const char *argv0)
+{
+	std::cerr << "usage: " << argv0 << " [file.vm or dir(with *.vm)]" << std::endl;
+	std::abort();
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+		abort_with_usage(argv[0]);
+
+	std::string arg_name(argv[1]);
+	fs::path arg_path(arg_name);
+
+	if (!fs::exists(arg_path))
+		abort_with_usage(argv[0]);
+
+	if (fs::is_directory(arg_path)) {
+		std::for_each(fs::directory_iterator(arg_path), fs::directory_iterator(),
+		              [](const fs::path &p) {
+			              if (p.extension() == ".vm")
+				              translate_file(p.string());
+		              });
+	} else if (fs::is_regular_file(arg_path)) {
+		if (arg_path.extension() == ".vm")
+			translate_file(arg_path.string());
+		else
+			abort_with_usage(argv[0]);
+	} else
+		abort_with_usage(argv[0]);
 
 	return 0;
 }
